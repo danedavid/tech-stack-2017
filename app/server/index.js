e749cf315f5c8f284a276a89d698eff6af57ec5e
dane <dane@qburst.com>
2017-11-03 16:10:03 +0530

    Set up npm scripts to serve

--- app/server/index.js
+++ app/server/index.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const PORT = 8000;
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+  console.log(`App listening on port http://localhost:${PORT}!`);
 });
